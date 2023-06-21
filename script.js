05e2298ee8015cb2068dacb96881101c1529d3da
Wiable <137183183+wiable@users.noreply.github.com>
2023-06-21 13:09:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,459 @@
+var subscriptionRadio = document.getElementById("calendly-subscription");
+var freeRadio = document.getElementById("free-calendly");
+var threePageSiteCheckbox = document.getElementById("three-page-site-info");
+var totalAmount = document.getElementById("total-amount");
+var fivePageSiteCheckbox = document.getElementById("five-page-site-info");
+var tenPageSiteCheckbox = document.getElementById("ten-page-site-info");
+var twentyPageSiteCheckbox = document.getElementById("twenty-page-site-info");
+var oneAdditionalPageCheckbox = document.getElementById("one-additional-page");
+var threeAdditionalPageCheckbox = document.getElementById(
+  "three-additional-page"
+);
+var fiveAdditionalPageCheckbox = document.getElementById(
+  "five-additional-page"
+);
+var aboutUsPresentationPageCheckbox = document.getElementById(
+  "about-us-presentation-page"
+);
+var teamPresentationPageCheckbox = document.getElementById(
+  "team-presentation-page"
+);
+var companyHistoryPresentationPageCheckbox = document.getElementById(
+  "company-history-presentation-page"
+);
+var additionalAboutUsPage = document.getElementById("additional-about-us-page");
+var additionalCompanyNewsPage = document.getElementById(
+  "additional-company-news-page"
+);
+var additionalPressPage = document.getElementById("additional-press-page");
+var additionalDirectSalePage = document.getElementById(
+  "additional-direct-sale-page"
+);
+var additionalAdsSpecialPage = document.getElementById(
+  "additional-advertising-special-page"
+);
+var additionalfedOptPage = document.getElementById(
+  "additional-feedback-option-page"
+);
+var additionalCreationNewsletter = document.getElementById(
+  "additional-creation-newsletter"
+);
+var additionalDiscussionForum = document.getElementById(
+  "additional-discussion-forum"
+);
+var additionalOneOnlineGame = document.getElementById(
+  "additional-one-online-game"
+);
+var additionalChatFunction = document.getElementById(
+  "additional-chat-function"
+);
+var additionalInteractionWeb2 = document.getElementById(
+  "additional-interaction-web2"
+);
+var additionalOfferFacebook = document.getElementById(
+  "additional-offer-facebook"
+);
+var additionalFAQ = document.getElementById("additional-faq");
+var additionalCaseStudies = document.getElementById("additional-case-studies");
+var additionalTutorials = document.getElementById("additional-tutorials");
+var additionalDownloadsPage = document.getElementById(
+  "additional-downloads-page"
+);
+var additionalMultilingualism = document.getElementById(
+  "additional-multilingualism"
+);
+var noDesignSystem = document.getElementById("have-design-system-no");
+var yesDesignSystem = document.getElementById("have-design-system-yes");
+var totalAmount = document.getElementById("total-amount");
+var fivePageSiteCheckbox = document.getElementById("five-page-site-info");
+var tenPageSiteCheckbox = document.getElementById("ten-page-site-info");
+var twentyPageSiteCheckbox = document.getElementById("twenty-page-site-info");
+var oneAdditionalPageCheckbox = document.getElementById("one-additional-page");
+var threeAdditionalPageCheckbox = document.getElementById(
+  "three-additional-page"
+);
+var fiveAdditionalPageCheckbox = document.getElementById(
+  "five-additional-page"
+);
+var aboutUsPresentationPageCheckbox = document.getElementById(
+  "about-us-presentation-page"
+);
+var teamPresentationPageCheckbox = document.getElementById(
+  "team-presentation-page"
+);
+var companyHistoryPresentationPageCheckbox = document.getElementById(
+  "company-history-presentation-page"
+);
+var additionalAboutUsPage = document.getElementById("additional-about-us-page");
+var additionalCompanyNewsPage = document.getElementById(
+  "additional-company-news-page"
+);
+var additionalPressPage = document.getElementById("additional-press-page");
+var additionalDirectSalePage = document.getElementById(
+  "additional-direct-sale-page"
+);
+var additionalAdsSpecialPage = document.getElementById(
+  "additional-advertising-special-page"
+);
+var additionalfedOptPage = document.getElementById(
+  "additional-feedback-option-page"
+);
+var additionalCreationNewsletter = document.getElementById(
+  "additional-creation-newsletter"
+);
+var additionalDiscussionForum = document.getElementById(
+  "additional-discussion-forum"
+);
+var additionalOneOnlineGame = document.getElementById(
+  "additional-one-online-game"
+);
+var additionalChatFunction = document.getElementById(
+  "additional-chat-function"
+);
+var additionalInteractionWeb2 = document.getElementById(
+  "additional-interaction-web2"
+);
+var additionalOfferFacebook = document.getElementById(
+  "additional-offer-facebook"
+);
+var additionalFAQ = document.getElementById("additional-faq");
+var additionalCaseStudies = document.getElementById("additional-case-studies");
+var additionalTutorials = document.getElementById("additional-tutorials");
+var additionalDownloadsPage = document.getElementById(
+  "additional-downloads-page"
+);
+var additionalMultilingualism = document.getElementById(
+  "additional-multilingualism"
+);
+var noTextImages = document.getElementById("have-text-images-no");
+var yesTextImages = document.getElementById("have-text-images-yes");
+var noDigitizedImages = document.getElementById("digitized-images-no");
+var yesDigitizedImages = document.getElementById("digitized-images-yes");
+var domainNo = document.getElementById("domain-no");
+var domainYes = document.getElementById("domain-yes");
+var noServer = document.getElementById("server-no");
+var yesServer = document.getElementById("server-yes");
+var noEmployee = document.getElementById("no-employee");
+var yesEmployee = document.getElementById("have-employee");
+var needPlan = document.getElementById("need-plan");
+var noneedPlan = document.getElementById("have-budget");
+var nowebsiteOnAll = document.getElementById("website-on-all-no");
+var yeswebsiteOnAll = document.getElementById("website-on-all-yes");
+var nowebsiteAppearsAll = document.getElementById("website-appears-all-no");
+var yeswebsiteAppearsAll = document.getElementById("website-appears-all-yes");
+var nowebsiteOnEsign = document.getElementById("website-on-e-sign-no");
+var yeswebsiteOnEsign = document.getElementById("website-on-e-sign-yes");
+var yesneedSeo = document.getElementById("need-seo-yes");
+var noneedSeo = document.getElementById("need-seo-no");
+var yesneedLinkExchange = document.getElementById("link-exchange-need-yes");
+var noneedLinkExchange = document.getElementById("link-exchange-need-yes");
+
+noDesignSystem.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+yesDesignSystem.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+noTextImages.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+yesTextImages.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+noDigitizedImages.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+yesDigitizedImages.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+domainNo.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+domainYes.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+noServer.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+yesServer.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+noEmployee.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+yesEmployee.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+needPlan.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+noneedPlan.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+nowebsiteOnAll.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+yeswebsiteOnAll.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+nowebsiteAppearsAll.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+yeswebsiteAppearsAll.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+nowebsiteOnEsign.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+yeswebsiteOnEsign.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+noneedSeo.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+yesneedSeo.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+yesneedLinkExchange.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+noneedLinkExchange.addEventListener("click", function () {
+  updateTotalAmount();
+});
+
+function updateTotalAmount() {
+  var amount = 0;
+
+  if (subscriptionRadio.checked || freeRadio.checked) {
+    amount = 500;
+  }
+
+  if (threePageSiteCheckbox.checked) {
+    amount += 600;
+  }
+
+  if (fivePageSiteCheckbox.checked) {
+    amount += 1000;
+  }
+
+  if (tenPageSiteCheckbox.checked) {
+    amount += 2000;
+  }
+
+  if (twentyPageSiteCheckbox.checked) {
+    amount += 4000;
+  }
+
+  if (oneAdditionalPageCheckbox.checked) {
+    amount += 200;
+  }
+
+  if (threeAdditionalPageCheckbox.checked) {
+    amount += 600;
+  }
+
+  if (fiveAdditionalPageCheckbox.checked) {
+    amount += 1000;
+  }
+
+  if (aboutUsPresentationPageCheckbox.checked) {
+    amount += 500;
+  }
+
+  if (teamPresentationPageCheckbox.checked) {
+    amount += 500;
+  }
+
+  if (companyHistoryPresentationPageCheckbox.checked) {
+    amount += 1000;
+  }
+
+  if (additionalAboutUsPage.checked) {
+    amount += 500;
+  }
+
+  if (additionalCompanyNewsPage.checked) {
+    amount += 500;
+  }
+
+  if (additionalPressPage.checked) {
+    amount += 500;
+  }
+
+  if (additionalDirectSalePage.checked) {
+    amount += 3500;
+  }
+
+  if (additionalAdsSpecialPage.checked) {
+    amount += 400;
+  }
+
+  if (additionalfedOptPage.checked) {
+    amount += 300;
+  }
+
+  if (additionalCreationNewsletter.checked) {
+    amount += 100;
+  }
+
+  if (additionalDiscussionForum.checked) {
+    amount += 600;
+  }
+
+  if (additionalOneOnlineGame.checked) {
+    amount += 600;
+  }
+
+  if (additionalChatFunction.checked) {
+    amount += 300;
+  }
+
+  if (additionalInteractionWeb2.checked) {
+    amount += 1000;
+  }
+
+  if (additionalOfferFacebook.checked) {
+    amount += 600;
+  }
+
+  if (additionalFAQ.checked) {
+    amount += 300;
+  }
+
+  if (additionalCaseStudies.checked) {
+    amount += 500;
+  }
+
+  if (additionalTutorials.checked) {
+    amount += 500;
+  }
+
+  if (additionalDownloadsPage.checked) {
+    amount += 500;
+  }
+
+  if (additionalMultilingualism.checked) {
+    amount += 500;
+  }
+
+  if (yesDesignSystem.checked) {
+    amount += 0;
+  }
+
+  if (noDesignSystem.checked) {
+    amount += 3000;
+  }
+
+  if (noTextImages.checked) {
+    amount += 400;
+  }
+
+  if (yesTextImages.checked) {
+    amount += 0;
+  }
+
+  if (noDigitizedImages.checked) {
+    amount += 2000;
+  }
+
+  if (yesDigitizedImages.checked) {
+    amount += 0;
+  }
+
+  if (domainNo.checked) {
+    amount += 200;
+  }
+
+  if (domainYes.checked) {
+    amount += 0;
+  }
+
+  if (noServer.checked) {
+    amount += 400;
+  }
+
+  if (yesServer.checked) {
+    amount += 0;
+  }
+
+  if (noEmployee.checked) {
+    amount += 1000;
+  }
+
+  if (yesEmployee.checked) {
+    amount += 0;
+  }
+
+  if (needPlan.checked) {
+    amount += 800;
+  }
+
+  if (noneedPlan.checked) {
+    amount += 0;
+  }
+
+  if (nowebsiteOnAll.checked) {
+    amount += 800;
+  }
+
+  if (yeswebsiteOnAll.checked) {
+    amount += 0;
+  }
+
+  if (nowebsiteAppearsAll.checked) {
+    amount += 1200;
+  }
+
+  if (yeswebsiteAppearsAll.checked) {
+    amount += 0;
+  }
+
+  if (nowebsiteOnEsign.checked) {
+    amount += 100;
+  }
+
+  if (yeswebsiteOnEsign.checked) {
+    amount += 0;
+  }
+
+  if (noneedSeo.checked) {
+    amount += 100;
+  }
+
+  if (yesneedSeo.checked) {
+    amount += 0;
+  }
+
+  if (yesneedLinkExchange.checked) {
+    amount += 300;
+  }
+
+  if (noneedLinkExchange.checked) {
+    amount += 0;
+  }
+
+  totalAmount.textContent = amount;
+}
